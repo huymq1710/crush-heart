d3f9e566dcf2bb7085562274b307a4b6d1182bf4
huy.mq1710 <huy17101999@gmail.com>
2021-08-11 21:36:35 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hello!!!!',
+    introDesc: ` T có điều này muốn nói với Cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Oke',
+    title: 'Cậu iu t có phải không 😙',
+    desc: 'Không trả lời hoặc thoát ra, tức là Cậu iu t đó nha :> ',
+    btnYes: 'Iu ơi là Iu <33',
+    btnNo: 'Mơ à :333',
+    question:'Nói cho t lý do vì sao iu đii <3',
+    btnReply: 'Gửi cho t <3',
+    reply: 'Tại vì cậu đẹp trai hihi <33333333',
+    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.instagram.com/huymq.1710/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
